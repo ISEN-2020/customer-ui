84e3e97a2510d3719f8ecbde0a700822eac714d0
Sweli0 <joris.ninfei@isen.yncrea.fr>
2024-09-06 15:01:24 +0200

    add trivy scanner

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,12 +4,13 @@ import bookService from '../services/bookService';
 const BookList = () => {
   const [books, setBooks] = useState([]);
 
+  // Récupère la liste des livres à l'initialisation du composant
   useEffect(() => {
     const fetchBooks = async () => {
       const response = await bookService.getBooks();
-      setBooks(response);
+      setBooks(response);  // Met à jour l'état avec les livres récupérés
     };
-    fetchBooks();
+    fetchBooks(); // Utilisation d'un tableau vide pour ne l'exécuter qu'une fois
   }, []);
 
   return (
