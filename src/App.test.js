bd26d2e12d4edf2ba8bdce7702c7d8ff73906909
maxence.abrt <mexence.aubert.int@bowwe.com>
2024-09-09 11:49:24 +0200

    add first test for index.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+  const linkElement = screen.getByText(/Bibliothèque Cloud Native/i);
+  
   expect(linkElement).toBeInTheDocument();
 });
